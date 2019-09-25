0441b0cd2d5c8d802ceb42f7135e9ec95e1d172b
nikhil <nikhiljain00019@gmail.com>
2019-09-25 20:37:38 +0530

    initialization of code

---
+++ stocks-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+         <h2>Welcome to Stocks App Live</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
